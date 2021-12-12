05e7d01b72b790f6cf87961eb38e019b91ad6e12
ArualFM <laura.fernandezmol@iesdoctorbalmis.com>
2021-12-12 22:52:36 +0100

    Añado los bindings y muestro las imagenes

--- JuegoPeliculas/Pelicula.cs
+++ JuegoPeliculas/Pelicula.cs
@@ -1,40 +1,42 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
-
-class Pelicula : ObservableObject
+namespace JuegoPeliculas
 {
-    private string titulo;
-    public string Titulo
+    class Pelicula : ObservableObject
     {
-        get { return titulo; }
-        set { SetProperty(ref titulo, value); }
-    }
+        private string titulo;
+        public string Titulo
+        {
+            get { return titulo; }
+            set { SetProperty(ref titulo, value); }
+        }
 
-    private string pista;
-    public string Pista
-    {
-        get { return pista; }
-        set { SetProperty(ref pista, value); }
-    }
+        private string pista;
+        public string Pista
+        {
+            get { return pista; }
+            set { SetProperty(ref pista, value); }
+        }
 
-    private string cartel;
-    public string Cartel
-    {
-        get { return cartel; }
-        set { SetProperty(ref cartel, value); }
-    }
+        private string cartel;
+        public string Cartel
+        {
+            get { return cartel; }
+            set { SetProperty(ref cartel, value); }
+        }
 
-    private string nivel;
-    public string Nivel
-    {
-        get { return nivel; }
-        set { SetProperty(ref nivel, value); }
-    }
+        private string nivel;
+        public string Nivel
+        {
+            get { return nivel; }
+            set { SetProperty(ref nivel, value); }
+        }
 
-    private string genero;
-    public string Genero
-    {
-        get { return genero; }
-        set { SetProperty(ref genero, value); }
+        private string genero;
+        public string Genero
+        {
+            get { return genero; }
+            set { SetProperty(ref genero, value); }
+        }
     }
 }
 
